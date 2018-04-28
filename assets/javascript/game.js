10bbc7ec2963d5030126de6023053bcc96e415fa
Your Name <jonathan.r.gilbert@gmail.com>
2018-04-28 13:15:21 -0700

    cleanup. increased text size and text positioning. revised questions and answers. added HTML tags to questions and answers. added a potential CSS fix for persistent highlighting on mobile.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    // on.click enable/disable swithc
+    // on.click enable/disable switch
     var click = 0;
 
     // timer variables
@@ -23,69 +23,69 @@ $(document).ready(function () {
     // Stores questions
     var quiz = [
         // books
-        "In the book 'Pride and Prejudice' (by Jane Austin), what adjective does Darcy first use to Describe Elizabeth?",
-        "In the book 'Alice and Wonderland' (by Lewis Caroll), Alice takes the Dutchess' baby outside, only to dsicover that it's a...",
-        "In the book 'Dracula' (by Brian Stoker), which of the following is NOT one of Dracula's powers?",
-        "In the book 'Animal Farm' (by George Orwell), the pigs' final procolomation states that 'All Animals are Equal but...",
+        "In the book <strong>Pride and Prejudice</strong> (by Jane Austin), what adjective does Darcy first use to describe Elizabeth?",
+        "In the book <strong>Alice and Wonderland</strong> (by Lewis Carroll), Alice takes the Duchess' baby outside, only to discover that it's a...",
+        "In the book <strong>Dracula</strong> (by Brian Stoker), which of the following is <strong>not</strong> one of Dracula's powers?",
+        "In the book <strong>Animal Farm</strong> (by George Orwell), the pigs' final proclamation states that &quot;All Animals are Equal but...",
         "Which of these famous movie monsters was adapted from a book written by a female author?",
         "Which of these famous sci-fi movie villains was adapted from a book written by a male author?",
         // poems
-        "In the poem 'The Raven' (by Edgar Allan Poe), the raven perches upon the bust of the god(dess) of...",
-        "In the poem 'The Jabberwocky' (by Lewis Caroll), the hero is warned to shun what creature?",
-        "In the poem 'Rime of the Ancient Mariner' (by Samuel Taylor Coolridge), who is the mariner telling his story to?",
-        "In the poem 'Kubla Kahn' (by Samuel Taylor Coolridge), Kubla Kahn's Pleasure Dome with many natural wonders. Including...",
+        "In the poem <strong>The Raven</strong> (by Edgar Allan Poe), the raven perches upon the bust of the god(dess) of...",
+        "In the poem <strong>The Jabberwocky</strong> (by Lewis Carroll), the hero is warned to shun what creature?",
+        "In the poem <strong>Rime of the Ancient Mariner</strong> (by Samuel Taylor Coleridge), to whom is the mariner telling his story?",
+        "In the poem <strong>Kubla Kahn</strong> (by Samuel Taylor Coleridge), Kubla Kahn's Pleasure Dome contains many wonders. Including...",
         "Which poem is also the title of a movie about its author?",
         "Which poem inspired the name of an elementary particle?",
         // graphic novels
-        "In the graphic novel series 'Locke and Key' (by Joe Hill), Lucas wins a fencing competition using what objects?",
-        "In the graphic novel series 'Scott Pilgrim' (by Brian Lee O'Maley), what is the name of Ramona's cat?",
-        "In the graphic novel 'Watchmen' (by Allan Moore), the phrase 'who watches the watchmen?' is a reference to...",
+        "In the graphic novel series <strong>Locke and Key</strong> (by Joe Hill), Lucas wins a fencing competition using what objects?",
+        "In the graphic novel series <strong>Scott Pilgrim</strong> (by Brian Lee O’Malley), what is the name of Ramona's cat?",
+        "In the graphic novel <strong>Watchmen</strong> (by Allan Moore), the phrase &quot;who watches the watchmen?&quot; is a reference to...",
         // film
-        "In the film 'The Martian,' Mark Watney (Matt Damon) vows to make Mars fear his...",
-        "In the film 'Young Frankenstein,' after shaking the Monster's hand, Inspector Kemp (Kenneth Mars) suggests everyone go to his house for...",
-        "In the film 'Mad Max: Fury Road,' Nux (Nicholas Hoult) has 2 growths on his neck. What are their names?",
-        "In the film 'The Shape of Water,' Elisa (Sally Hawkins) signs a series of letters to Colonel Strickland. What is the 3rd letter?",
-        "In the film 'The Disaster Artist,' what book do Tommy and Greg act out at a diner?",
+        "In the film <strong>The Martian</strong>, Mark Watney (Matt Damon) vows to make Mars fear his...",
+        "In the film <strong>Young Frankenstein</strong>, after shaking the Monster's hand, Inspector Kemp (Kenneth Mars) suggests everyone go to his house for...",
+        "In the film <strong>Mad Max: Fury Road</strong>, Nux (Nicholas Hoult) has 2 growths on his neck. What are their names?",
+        "In the film <strong>The Shape of Water</strong>, Elisa (Sally Hawkins) repeatedly signs a series of letters to Colonel Strickland. What is the 3rd letter?",
+        "In the film <strong>The Disaster Artist</strong>, what book do Tommy and Greg act out at a diner?",
         // TV
-        "In the TV series 'Dilbert,' Jerry Seinfeld provides the voice of what character?",
-        "In the TV series 'Avatar: the Last Airbender,' what is the name of the creature that steals faces?",
-        "In the TV series 'Seinfeld,' Jerry claims that a comedian dating a woman who never laughts is like...",
-        "In the TV series 'Archer,' what are the names of the two hitmen who attackm Archer and Ramon, on behalf of Cuba?",
-        "In the TV series 'Futurama,' what 'Twilight Zone' episode is NOT referenced in a 'Scary Door' episode?"
+        "In the TV series <strong>Dilbert</strong>, Jerry Seinfeld provides the voice of what character?",
+        "In the TV series <strong>Avatar: The Last Airbender</strong>, what is the name of the creature that steals faces?",
+        "In the TV series <strong>Seinfeld</strong>, Jerry claims that a comedian dating a woman who never laughs is like...",
+        "In the TV series <strong>Archer</strong>, what are the names of the two hitmen who attack Archer and Ramon, on behalf of Cuba?",
+        "In the TV series <strong>Futurama</strong>, what <strong>Twilight Zone</strong> episode is <strong>not</strong> referenced in a <strong>Scary Door</strong>	episode?"
     ];
 
-    // Stores explanations (listed in order to matchquestions; explanation for quiz.1 is answer.1)
+    // Stores explanations (listed to match questions; explanation for quiz.1 is answer.1)
     var answer = [
         // books        
-        "Darcy says that Elizabeth is 'TOLERABLE, but not handsome enough to tempt me.'",
-        "Alice discovers that the baby is a pig after taking it outside.",
-        "Dracula did not have the power to raise zombies.",
-        "'All Animals are Equal, but Some Animals are More Equal Than Others.'",
-        "The book 'Frankenstein' was originally written by Marry Shelley.",
-        "The book '2001: A Space Odyssey,' which intorduced the HAL-9000, was written by Arthur C. Clarke.",
+        "Darcy says that Elizabeth is &quot;<strong>tolerable</strong>, but not handsome enough to tempt me.&quot;",
+        "Alice takes the baby outside, only to realize it has become a <strong>pig</strong>.",
+        "Dracula did <strong>not</strong> have the power to <strong>raise zombies</strong>.",
+        "&quot;All Animals are Equal, <strong>but Some Animals are More Equal Than Others</strong>.&quot;",
+        "The book <strong>Frankenstein</strong> was originally written by Marry Shelley.",
+        "The book <strong>2001: A Space Odyssey</strong>, which introduced <strong>the HAL-9000</strong>, was written by Arthur C. Clarke.",
         // poems
-        "The raven purches upon a bust of Pallas Athena, the Greek/Roman goddess of wisdom, handicraft, and war.",
-        "The hero is warned to 'shun the frumious Bandersnatch.'",
-        "The mariner is telling his story to a wedding guest.",
-        "'It was a miracle of rare device / a sunny Pleasure Dome with caves of ice.'",
-        "'Bright Star' is the title of a poem by John Keats, and the title of a movie about John Keats.",
-        "'Quark' is a nonsense word in 'Finnegans Wake,' and the name of an elementary particle in real life.",
+        "The raven perches upon a bust of Pallas Athena, the Greek/Roman goddess of wisdom, handicraft, and <strong>war</strong>.",
+        "The hero is warned to &quot;shun the frumious <strong>Bandersnatch</strong>.&quot;",
+        "The mariner is telling his story to <strong>a wedding guest</strong>.",
+        "&quot;It was a miracle of rare device / a sunny Pleasure Dome with <strong>caves of ice</strong>.&quot;",
+        "<strong>Bright Star</strong> is the title of a poem by John Keats, and the title of a movie about John Keats.",
+        "<strong>&quot;Quark&quot;</strong> is a nonsense word in <strong>Finnegan’s Wake</strong>, and the name of an elementary particle in real life.",
         // graphic novels
-        "Lucas is able to gain instant, absolute knowledge of fencing by opening his head with the Head Key and dropping a Fencing Instructional Handbook into his mind.",
-        "Strangely, Ramona named her cat after her worst ex-boyfriend: Gideon.",
-        "The phrase is a translation of the Latin 'quis custodiet ipsos custodes?', made famous by the ancient 'Satires' of Juvenal.'",
+        "To master fencing, simply open your head with the <strong>Head Key</strong> and drop a <strong>Fencing Handbook</strong> into your brian.",        
+        "Strangely, Ramona named her cat after her worst ex-boyfriend: <strong>Gideon</strong>.",
+        "The phrase is a translation of the Latin &quot;quis custodiet ipsos custodes?&quot; The phrase was made famous by the ancient <strong>Satires of Juvenal.</strong>",
         // film
-        "Mark confidently states 'Mars will come to fear my botany powers.'",
-        "After acknowledging the monster's humanity, Kemp invites everyone to his place 'for a little sponge cake und a little wine.'",
-        "Nux affectionately refers to his two tumors as 'Larry & Berry.'",
-        "Elisa repeatedly signs 'f*** you' to Strickland, one letter at a time. Letter #3 is 'C.'",
-        "Tommy and Greg read aloud a section of Herman Melville's 'The Lightning-Rod Man.'",
+        "&quot;Mars will come to fear my <strong>botany powers</strong>.&quot;",
+        "After acknowledging the monster's humanity, Kemp invites everyone to his place &quot;for a little <strong>sponge cake</strong> und a little <strong>wine</strong>.&quot;",
+        "Nux affectionately refers to his two tumors as <strong>&quot;Larry & Berry.&quot;</strong>",
+        "Elisa repeatedly signs <strong>&quot;F*** you&quot;</strong> to Strickland, one letter at a time. Making letter #3 is <strong>&quot;C.&quot;</strong>",
+        "Tommy and Greg read aloud a section of Herman Melville's <strong>The Lightning-Rod Man</strong>",
         // TV
-        "Seinfeld lends his voice to Comp-U-Comp, a sentient computer in charge of a shipping company.",
-        "Koh is a creature in the spirit world who steals faces which show emotion.",
-        "Jerry tells Elaine that 'it's like...well, it's like something.'",
-        "Charles and Rudy are the hitman who Archer and Ramon run accross.",
-        "'It's a Good Life' (arguably the most well-known 'Twilgiht Zone' episode) is not referneced by the 'Scary Door.'"
+        "Jerry Seinfeld lends his voice to <strong>Comp-U-Comp</strong>, a sentient computer in charge of a shipping company.",
+        "Never show emotion to <strong>Koh</strong>. If you do, he'll steal your face.",
+        "&quot;It's like...well, it's like <strong>something</strong>.&quot;",
+        "<strong>Charles and Rudy</strong> are the hitmen who Archer and Ramon run across.",
+        "<strong>It's a Good Life</strong> (arguably the most well-known <strong>Twilight Zone</strong> episode) is not referenced by the <strong>Scary Door.</strong>"
     ];
 
     // Stores correct answers (listed in order to match questions; correct answer for quiz.1 is key.1)
@@ -128,32 +128,32 @@ $(document).ready(function () {
         "Tolerable",
         "Doll",
         "Teleportation",
-        "...Some Animals are More Equal than Others.'",
-        "Frankenstein",
+        "...Some Animals are More Equal than Others.&quot;",
+        "Frankenstein's Monster",
         "Doc Brown",
         // poems
         "Death",
         "Jubjub Bird",
-        "Navigator",
+        "A Navigator",
         "Fields of Amber",
-        "'Eve of St. Agnes'",
-        "'Ode to a Grecian Urn'",
+        "<strong>Eve of St. Agnes</strong>",
+        "<strong>Ode to a Grecian Urn</strong>",
         // graphic novels
         "The Music Key, a Music Box, and Earplugs",
         "Gideon",
-        "'Essay Concerning Human Understanding' by John Locke",
+        "<strong>Essay Concerning Human Understanding</strong> by John Locke",
         // film
         "Engineering Savvy",
         "Champagne and Cigars",
         "Larry & Berry",
         "L",
-        "'Do Androids Dream of Electric Sheep?'",
+        "<strong>Do Androids Dream of Electric Sheep?</strong>",
         // TV
         "Bob Bastard",
         "Raava",
         "Something",
         "Charles & Rudy",
-        "'Nightmare at 20,000 Feet'",
+        "<strong>Nightmare at 20,000 Feet</strong>",
     ];
 
     // B category choices; B for quiz.1 is B.1)
@@ -162,32 +162,32 @@ $(document).ready(function () {
         "Ravishing",
         "Tiny Adult",
         "Controlling the Weather",
-        "...All Must Live in Service to Pigs.'",
+        "...All Must Live in Service to Pigs.&quot;",
         "Dracula",
         "HAL-9000",
         // poems
-        "Saddness",
+        "Sadness",
         "Jabberwock",
-        "Wedding Guest",
+        "A Wedding Guest",
         "Forests of Yew",
-        "'Ode to Psyche'",
-        "'Finnegans Wake'",
+        "<strong>Ode to Psyche</strong>",
+        "<strong>Finnegan’s Wake</strong>",
         // graphic novels
         "The Timeshift Key and a Stopwatch",
         "Lobo",
-        "'Also sprach Zarathustra' by Friedrich Nietzsche",
+        "<strong>Also sprach Zarathustra</strong> by Friedrich Nietzsche",
         // film
         "Botany Powers",
         "Sponge Cake and Wine",
         "Jerry & Merry",
         "S",
-        "'Moby Dick'",
+        "<strong>Moby Dick</strong>",
         // TV
         "Catbert",
         "Wan Shi Tong",
         "Newman Dating a Dog",
         "La Madrina & Juliana",
-        "'It's a Good Life'",
+        "<strong>It's a Good Life</strong>",
     ];
 
     // C category choices; C for quiz.1 is C.1)
@@ -196,66 +196,66 @@ $(document).ready(function () {
         "Talkative",
         "Pig",
         "Animating Zombies",
-        "...Equality Must be Earned.'",
+        "...Equality Must be Earned.&quot;",
         "Xenomorph Queen",
         "Xenomorph Queen",
         // poems
         "War",
         "Bandersnatch",
-        "Fisherman",
+        "A Fisherman",
         "Caves of Ice",
-        "'Fall of Hyperion'",
-        "'The Eolian Harp'",
+        "<strong>Fall of Hyperion</strong>",
+        "<strong>The Eolian Harp</strong>",
         // graphic novels
         "The Hercules Key and Winged Boots",
         "Frank",
-        "'Julias Ceaser' by William Shakespeare",
+        "<strong>Julius Ceaser</strong> by William Shakespeare",
         // film
         "Cosmic Stubbornness",
-        "Poker and Coffe",
+        "Poker and Coffee",
         "Mella & Gnoma",
         "N",
-        "'The Lightning-Rod Man'",
+        "<strong>The Lightning-Rod Man</strong>",
         // TV
         "Comp-U-Comp",
         "Tui",
         "A Chef Dating an Anorexic",
         "Manfred & Uta",
-        "'The Man in the Bottle'",
+        "<strong>The Man in the Bottle</strong>",
     ];
 
     // D category choices; D for quiz.1 is D.1)
     var d = [
         // books
-        "Aggrevating",
+        "Aggravating",
         "Jabberwock",
         "Shapeshifting",
-        "...Must Still Serve the Greater Society.'",
+        "...Must Still Serve the Greater Society.&quot;",
         "Wolf-Man",
         "The Terminator",
         // poems
         "Night",
         "Tumtum Tree",
-        "Writer",
+        "A Writer",
         "Walls of Pure Sunlight",
-        "'Bright Star'",
-        "'The Phoenix and the Turtle'",
+        "<strong>Bright Star</strong>",
+        "<strong>The Phoenix and the Turtle</strong>",
         // graphic novels
         "The Head Key and a Book",
         "Germaine",
-        "'Satires of Juvenal' by Juvenal",
+        "<strong>Satires of Juvenal</strong> by Juvenal",
         // film
         "Astrophysics Mastery",
         "Beer and Skeet Shooting",
         "Harry & Sally",
         "C",
-        "'The Sound and the Fury'",
+        "<strong>The Sound and the Fury</strong>",
         // TV
         "Toby",
         "Koh",
         "A Dentist Dating an Anti-Dentite",
         "Spelvin & Keiko",
-        "'Time Enough to Last'",
+        "<strong>Time Enough to Last</strong>",
     ];
 
     // picks a question (from the list in the quiz variable)
@@ -275,7 +275,7 @@ $(document).ready(function () {
     // resets stored questions (repeats are possible over the course of multiple games, but not during the same game)
     // NOTE: for this to work, quiz.length must always be divisible by 5 (without decimals) 
     function reset() {
-        log.length = 0;
+        log = [];
     };
 
     // populates divs with relevant info, appends choice list, removes 'start' button
@@ -331,10 +331,34 @@ $(document).ready(function () {
         }
     };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
     // 5 second timer (for answers)
     function timer2() {
         flavorize ();
-        var timer2 = 5;
+        var timer2 = 10;
         stopwatch2 = setInterval(function () {
             timer2--;
             if (timer2 >= 0) {
@@ -454,7 +478,7 @@ $(document).ready(function () {
                 $("#banner").addClass("green").removeClass("red").removeClass("blue");
                 $("#banner").html("Correct!");
                 span = document.getElementById("timer");
-                span.innerHTML = "5";
+                span.innerHTML = "10";
                 clearInterval(stopwatch);
                 timer2();
             }
@@ -463,7 +487,7 @@ $(document).ready(function () {
                 $("#banner").addClass("red").removeClass("blue").removeClass("green");
                 $("#banner").html("Incorrect!");
                 span = document.getElementById("timer");
-                span.innerHTML = "5";
+                span.innerHTML = "10";
                 clearInterval(stopwatch);
                 timer2();
             }
